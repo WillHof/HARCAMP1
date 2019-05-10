63f92f2194fc98ebf7123bcae3fc358ea178c98e
Alvaro Lopes <2452169+Alvie40@users.noreply.github.com>
2019-05-10 09:20:22 -0400

    add message modal to login

--- login/scripts/auth.js
+++ login/scripts/auth.js
@@ -50,19 +50,52 @@ loginForm.addEventListener('submit', (e) => {
   const email = loginForm['login-email'].value;
   const password = loginForm['login-password'].value;
 
+  // log the user in ------ with messages
+   
   // log the user in
-  auth.signInWithEmailAndPassword(email, password)
-    .then((cred) => {
-      // close the signup modal & reset form
-      cred.user.uid = userID
-      const modal = document.querySelector('#modal-login');
-
-      M.Modal.getInstance(modal).close();
-
-      loginForm.reset();
-    })
-    .catch(err => {
-      alert(err.message);
-    });
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    // close the signup modal & reset form
+    cred.user.uid = userID
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+    if (userID){
+      messageWelcome("Welcome to Intercar!!!");
+    }
+  }).catch(function(error) {
+    // Handle Errors here.
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+    messageWelcome("Sorry, it appears your ID/Password are wrong");
+    // ...
+  });
 
 });
+
+// Modal Message
+function messageWelcome(displayText) {
+// Get the modal message
+var modal = document.getElementById('modal-message');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var displayMessage = document.getElementById('display-text');
+displayMessage.innerHTML=displayText;
+// Open the modal 
+  modal.style.display = "block";
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+}
